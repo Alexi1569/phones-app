83d01d6cd23f63c9517c2f9df92689bb9aa49b13
Alexi1569 <farikprof@Gmail.com>
2019-03-26 15:51:55 +0200

    popup processing

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -6,9 +6,64 @@ import { ButtonContainer } from './Button';
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h1>hello from details</h1>
-      </div>
+      <ProductConsumer>
+        {ctx => {
+          const {
+            id,
+            company,
+            title,
+            img,
+            price,
+            info,
+            inCart
+          } = ctx.detailProduct;
+
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} alt={title} className="img-fluid" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>model : {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    made by : <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      price: <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    some info about product:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer>Back to products</ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                      cart
+                      disabled={inCart}
+                      onClick={() => {
+                        ctx.addToCart(id);
+                      }}
+                    >
+                      {inCart ? 'inCart' : 'add to cart'}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
