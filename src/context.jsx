9ebce031e8db693021676d8443fe43104c49801b
Alexi1569 <farikprof@Gmail.com>
2019-03-26 11:50:05 +0200

    product page ready

---
+++ src/context.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+  state = {
+    products: [],
+    detailProduct
+  };
+
+  handleDetail = () => {
+    console.log('hello from detail');
+  };
+
+  addToCart = () => {
+    console.log('hello from addt to cart');
+  };
+
+  setProducts = () => {
+    let products = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+
+    this.setState(() => {
+      return { products };
+    });
+  };
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  render() {
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer };
