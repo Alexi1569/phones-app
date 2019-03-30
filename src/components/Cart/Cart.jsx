1d4645bc3a65a64be256a83034bb4add25254ac7
Alexi1569 <farikprof@gmail.com>
2019-03-30 16:59:19 +0200

    markup cart ready

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,7 @@ import CartColumns from './CartColumns';
 import EmptyCart from './EmptyCart';
 import { ProductConsumer } from '../../context';
 import CartList from './CartList';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
   render() {
@@ -19,6 +20,7 @@ export default class Cart extends Component {
                   <Title name='your' title='cart' />
                   <CartColumns />
                   <CartList ctx={ctx} />
+                  <CartTotals ctx={ctx} />
                 </React.Fragment>
               );
             } else {
